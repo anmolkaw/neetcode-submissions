45ff7b0df2c7fd2ec71cd097002579b75b38e8de
Anmol Kaw <149358577+anmolkaw@users.noreply.github.com>
2026-05-26 20:17:46 +0530

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        if(s.size() != t.size()){
+            return false;
+        }
+
+        unordered_map<char, int> mp;
+
+        for(char ch:s){
+            mp[ch]++;
+        }
+
+        int count = t.size();
+
+        for(char ch:t){
+            if(mp.find(ch) != mp.end() && mp[ch] > 0){
+                mp[ch]--;
+                count--;
+            }
+        }
+
+        if(count == 0){
+            return true;
+        }
+
+
+        return false;
+
+    }
+};
