3e14f1cf8d166ac63e6b25729aa6fd9799751a9d
Anmol Kaw <149358577+anmolkaw@users.noreply.github.com>
2026-06-29 11:59:46 +0530

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.cpp
@@ -0,0 +1,26 @@
+// Optimized Solution
+// Time Complexity - O(n)
+// Space Complexity - O(1)
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int left = 0;
+        int right = 1;
+        int maxProfit = 0;
+
+        while(right < prices.size()){
+           int profit = prices[right] - prices[left];
+
+            maxProfit = max(maxProfit, profit);
+            if(profit < 0){
+                left = right;
+                right++;
+            }else{
+                right++;
+            }
+        }
+
+        return maxProfit;
+    }
+};
