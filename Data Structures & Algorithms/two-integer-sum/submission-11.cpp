0a0487b471b6722e8f95035a4116e69f36168871
Anmol Kaw <149358577+anmolkaw@users.noreply.github.com>
2026-07-09 13:51:41 +0530

    Add: two-integer-sum - submission-11

---
+++ Data Structures & Algorithms/two-integer-sum/submission-11.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        int n = nums.size();
+        for(int i=0;i<nums.size();i++){
+            int compliment = target - nums[i];
+            if(mp.find(compliment) != mp.end()){
+                return {mp[compliment], i};
+            }
+
+            mp[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
