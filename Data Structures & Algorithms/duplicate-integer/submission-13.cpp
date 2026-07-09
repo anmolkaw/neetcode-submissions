db4c5ce44837ee0bcaa6999f748a840e06bc78c7
Anmol Kaw <149358577+anmolkaw@users.noreply.github.com>
2026-07-09 13:19:02 +0530

    Add: duplicate-integer - submission-13

---
+++ Data Structures & Algorithms/duplicate-integer/submission-13.cpp
@@ -0,0 +1,16 @@
+// Optimized solution
+
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> seen;
+        for(int num : nums){
+            if(seen.count(num)){
+                return true;
+            }
+            seen.insert(num);
+        }
+
+        return false;
+    }
+};
