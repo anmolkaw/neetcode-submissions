fc5e74b6fa15ca3d90e79cb4f4f974cd5a6a53f3
Anmol Kaw <149358577+anmolkaw@users.noreply.github.com>
2026-07-13 22:43:44 +0530

    Add: majority-element - submission-7

---
+++ Data Structures & Algorithms/majority-element/submission-7.cpp
@@ -0,0 +1,18 @@
+// Hashmap Approach
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> mp;
+
+        for(int num:nums){
+            mp[num]++;
+
+            if(mp[num] > nums.size()/2){
+                return num;
+            }
+        }
+
+        return -1;
+    }
+};
